07175122abfe8547d0ae3cef31f7217f79a2392f
MDRomzan <mdromjan483@gmail.com>
2021-10-26 15:56:15 +0600

    first commit

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,11 +1,20 @@
 console.log(process.env);
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
+    // apiKey: process.env.REACT_APP_API_KEY,
+    // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    // projectId: process.env.REACT_APP_PROJECT_ID,
+    // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    // appId: process.env.REACT_APP_APP_ID,
+    
+  apiKey: "AIzaSyCMJIcQFKMtvcE-K_Pzrdax5Arwg7mp9TA",
+  authDomain: "ema-jonn-authentication-6da0f.firebaseapp.com",
+  projectId: "ema-jonn-authentication-6da0f",
+  storageBucket: "ema-jonn-authentication-6da0f.appspot.com",
+  messagingSenderId: "362925547623",
+  appId: "1:362925547623:web:b335a6a3a69bc14fd8b4c2"
+
+
 };
 
 export default firebaseConfig;
